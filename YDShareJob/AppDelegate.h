2906415701c687ca0f0e041d800a1d85cc807cf3
李加建 <779454060@qq.com>
2017-10-17 10:36:16 +0800

    Initial Commit

---
+++ YDShareJob/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YDShareJob
+//
+//  Created by 李加建 on 2017/10/17.
+//  Copyright © 2017年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
